05f8de675b64db35d3c97256f31ea684862d01f9
ritikmaheshwari123 <maheshwariritik85@gmail.com>
2025-09-22 11:40:44 +0530

    Commnets added for previous feature

--- currency-conversion-service/src/main/java/com/microservice/currency_conversion_service/proxy/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/microservice/currency_conversion_service/proxy/CurrencyExchangeServiceProxy.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
+//@FeignClient(name="exchange-service", url="localhost:8001")
 @FeignClient(name="exchange-service")
 @RibbonClient(name="exchange-service")
 public interface CurrencyExchangeServiceProxy {
