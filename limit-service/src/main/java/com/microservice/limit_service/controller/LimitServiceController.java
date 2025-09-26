c73ea445bd0650766e27e93321ba5a4f73f0cb04
ritikmaheshwari123 <maheshwariritik85@gmail.com>
2025-09-26 15:54:15 +0530

    Spring Cloud Netflix hystrix

--- limit-service/src/main/java/com/microservice/limit_service/controller/LimitServiceController.java
+++ limit-service/src/main/java/com/microservice/limit_service/controller/LimitServiceController.java
@@ -2,6 +2,7 @@ package com.microservice.limit_service.controller;
 
 import com.microservice.limit_service.configuration.Configuration;
 import com.microservice.limit_service.model.LimitServiceConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,23 @@ public class LimitServiceController {
     private Configuration configuration;
 
     @GetMapping("/limit-service")
+    @HystrixCommand(fallbackMethod = "reliable")
     public LimitServiceConfiguration retrieveLimitsFromConfiguration() {
 
         return new LimitServiceConfiguration(configuration.getMinimum(), configuration.getMaximum());
     }
+
+    @GetMapping("limit-service-hystrix")
+    @HystrixCommand(fallbackMethod = "reliable")
+    public LimitServiceConfiguration retievelimitexchangeconfiguration()
+    {
+
+        throw new RuntimeException("****Service Error Exception********");
+    }
+
+    public LimitServiceConfiguration reliable() {
+        return new LimitServiceConfiguration(1,1);
+    }
+
+
 }
